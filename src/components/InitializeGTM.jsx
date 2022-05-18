d96bd8ade9250b3365308636c6ecef196e20ad22
Kendall Christy <102176349+kendallchristy@users.noreply.github.com>
2022-05-18 11:36:51 -0400

    Uploading in the right spot

--- src/components/InitializeGTM.jsx
+++ src/components/InitializeGTM.jsx
@@ -5,101 +5,118 @@ const InitializeGTM = ( props ) => {
 
   useEffect(() => {
 
-    if (!mx.ga4Connected) { // we need the flag because onNavigation is called several times for things like after widget load, after show page, etc. If we remove the flag, then we send more than one page hit per page viewing
-      const origOnNavigation = mx.ui.getContentForm().onNavigation; // cache what the original function did
-      mx.ui.getContentForm().onNavigation = () => {
-        // Complete custom actions
-        debugger;
-        if (props.sendCustomProps === false) {
-          // onNavigation, flag goes in mx.<something>
-          const tagManagerArgsInitialize = {
-            gtmId: props.measurementID
-          }
-
-          TagManager.initialize(tagManagerArgsInitialize)
+    if (!mx.ga4Connected) { // flag to prevent onNavigation from sending multiple page views after things like after widget load, after show page, etc.
+      
+      // dataUnavailable check here because the render function will take care of calling it multiple times
+      let dataUnavailable = false;
+      for (let object of props.additionalProps) {
+        if (object.propDataSource.status !== 'available') {
+          dataUnavailable = true;
         }
-        
-        else {
-          //
+      }
 
-          let dataLayerStructure = function(props) {
-            var dataLayer = '{"event":"' + props.pageViewEventName + '",';
+      if (!dataUnavailable) {
 
-            if (props.sendPageTitle) {
-              dataLayer += '"Page Name":"' + mx.ui.getContentForm().title + '",';
+        const origOnNavigation = mx.ui.getContentForm().onNavigation; // save what the original onNavigation function did
+        mx.ui.getContentForm().onNavigation = () => {
+          
+          // Custom Actions
+          if (props.sendCustomProps === false) {
+            const tagManagerArgsInitialize = {
+              gtmId: props.measurementID
             }
 
-            if (props.sendModuleLocation) {
-              var modulePath = mx.ui.getContentForm().path;
-              var moduleLocation = (function(modulePath) {
-                var pageExtension = ".page.xml";
-                var path = modulePath.substr(0, modulePath.length - pageExtension.length);
-                return path;
-              }(modulePath));
-              dataLayer += '"Module Location":"' + moduleLocation + '",';
-            }
+            TagManager.initialize(tagManagerArgsInitialize)
+          }
+          
+          else {
+            let dataLayerStructure = function(props) {
+              var dataLayer = '{"event":"' + props.pageViewEventName + '",'; // initialize the dataLayer variable
 
-            if (props.sendPageURL) {
-              if (mx.ui.getContentForm().url !== null) {
-                var pageURL = window.location.origin + mx.ui.getContentForm().url;
+              if (props.sendPageTitle) { // send page title
+                dataLayer += '"Page Name":"' + mx.ui.getContentForm().title + '",';
+              }
+
+              if (props.sendModuleLocation) { // send module location
+                var modulePath = mx.ui.getContentForm().path;
+                var moduleLocation = (function(modulePath) {
+                  var pageExtension = ".page.xml";
+                  var path = modulePath.substr(0, modulePath.length - pageExtension.length);
+                  return path;
+                }(modulePath));
+                dataLayer += '"Module Location":"' + moduleLocation + '",';
+              }
+
+              if (props.sendPageURL) { // send page URL
+                if (mx.ui.getContentForm().url !== null) {
+                  var pageURL = window.location.origin + mx.ui.getContentForm().url;
+                }
+                
+                else {
+                  var pageURL = window.location.origin;
+                }
+
+                var trimmedURL = (function(fullURL) {
+                  var lastCharIndex = fullURL.lastIndexOf("/");
+                  var endString = fullURL.substring(lastCharIndex + 1,fullURL.length);
+                  if (isNaN(endString)) {
+                    return fullURL; // the end of the string isn't a number, return the whole thing
+                  }
+                  else {
+                    return fullPath.substr(0,lastCharIndex); // the end of the string is a number, trim it
+                  }
+                }(pageURL));
+                dataLayer += '"Page URL":"' + trimmedURL + '",';
+              }
+
+              if (props.sendSessionID) { // send session ID
+                dataLayer += '"Session ID":"' + mx.session.getSessionObjectId() + '",';
+              }
+
+              if (props.sendAdditionalProps) { // send additional properties
+                let expressionResult= '';
+
+                for (let line of props.additionalProps) {
+                  for (let object of line.propDataSource.items) { // object is an item in the list that is returned from the data source
+                    expressionResult += line.propValue.get(object).value + ', ';
+                  }
+
+                  expressionResult = expressionResult.replace(/,\s*$/, "");
+                  dataLayer += '"' + line.propName + '":"' + expressionResult + '",';
+                  expressionResult = '';
+                }
               }
               
-              else {
-                var pageURL = window.location.origin;
-              }
-
-              var trimmedURL = (function(fullURL) {
-                var lastCharIndex = fullURL.lastIndexOf("/");
-                var endString = fullURL.substring(lastCharIndex + 1,fullURL.length);
-                if (isNaN(endString)) {
-                  return fullURL; // the end of the string isn't a number, return the whole thing
-                }
-                else {
-                  return fullPath.substr(0,lastCharIndex); // the end of the string is a number, trim it
-                }
-              }(pageURL));
-              dataLayer += '"Page URL":"' + trimmedURL + '",';
+              dataLayer = dataLayer.replace(/,\s*$/, ""); // remove the last comma from the dataLayer variable
+              dataLayer += "}"
+              return JSON.parse(dataLayer);
             }
 
-            if (props.sendSessionID) {
-              dataLayer += '"Session ID":"' + mx.session.getSessionObjectId() + '",';
+            let dataLayer = dataLayerStructure(props);
+
+            const tagManagerArgs = {
+              gtmId: props.measurementID,
+              dataLayer
             }
 
-            if (props.sendAdditionalProps) {
-              for (const [propName, propValue] of Object.entries(props.additionalProps)) {
-              dataLayer += '"' + propValue.propName + '":"' + propValue.propValue.value + '",';
-              }
-            }
-            
-            dataLayer = dataLayer.replace(/,\s*$/, ""); // remove the last comma
-            dataLayer += "}"
-            return JSON.parse(dataLayer);
+            TagManager.initialize(tagManagerArgs)
           }
 
-          let dataLayer = dataLayerStructure(props);
+          mx.ga4Connected = true; // We have sent the page hit, now toggle the switch off to avoid sending duplicates
 
-          const tagManagerArgs = {
-            gtmId: props.measurementID,
-            dataLayer
-          }
-          TagManager.initialize(tagManagerArgs)
+          // Original Actions
+          origOnNavigation();
+        
+
+          return () => {
+            null;
+          } 
         }
-
-        //mx.previousLayout = mx.ui.getContentForm()._currentLayouts[0]; // set the previous layout after the page has loaded for the next comparison
-        mx.ga4Connected = true;
-
-        // Complete original actions
-        origOnNavigation();
-
-        return () => {
-          null;
-        } 
       }
     }
-  }, []);
+  }); // no dependency array -> trigger on every load
 
   return null;
-
 }
 
 export default InitializeGTM
