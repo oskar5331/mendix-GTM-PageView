fc3145935434084628e4c24a57df9f5704547706
Kendall Christy <102176349+kendallchristy@users.noreply.github.com>
2022-05-18 10:37:16 -0500

    Delete GoogleTagManager.jsx

--- GoogleTagManager.jsx
+++
@@ -1,6 +0,0 @@
-import { createElement } from "react";
-import InitializeGTM from "./components/InitializeGTM";
-
-export function GoogleTagManager({ measurementID, sendCustomProps, pageViewEventName, sendPageTitle, sendModuleLocation, sendPageURL, sendSessionID, sendAdditionalProps, additionalProps }) {
-    return <InitializeGTM measurementID={measurementID} sendCustomProps={sendCustomProps} pageViewEventName={pageViewEventName} sendPageTitle={sendPageTitle} sendModuleLocation={sendModuleLocation} sendPageURL={sendPageURL} sendSessionID={sendSessionID} sendAdditionalProps={sendAdditionalProps} additionalProps={additionalProps} />;
-}
