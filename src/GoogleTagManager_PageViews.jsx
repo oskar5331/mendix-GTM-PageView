0bf2380001b1468d410205bf5d29f79237fa4f6e
Kendall Christy <102176349+kendallchristy@users.noreply.github.com>
2022-06-29 14:10:31 -0600

    Updating the build file to have a new package name

---
+++ src/GoogleTagManager_PageViews.jsx
@@ -0,0 +1,28 @@
+import InitializeGTM from "./components/InitializeGTM";
+import { createElement } from "react";
+
+export function GoogleTagManager({
+    measurementID,
+    sendCustomProps,
+    pageViewEventName,
+    sendPageTitle,
+    sendModuleLocation,
+    sendPageURL,
+    sendSessionID,
+    sendAdditionalProps,
+    additionalProps
+}) {
+    return (
+        <InitializeGTM
+            measurementID={measurementID}
+            sendCustomProps={sendCustomProps}
+            pageViewEventName={pageViewEventName}
+            sendPageTitle={sendPageTitle}
+            sendModuleLocation={sendModuleLocation}
+            sendPageURL={sendPageURL}
+            sendSessionID={sendSessionID}
+            sendAdditionalProps={sendAdditionalProps}
+            additionalProps={additionalProps}
+        />
+    );
+}
